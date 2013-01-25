ed4fdb19dd400c169d07b11e6ed24eb5bb731e6b
Stephen Jennings <Stephen.G.Jennings@gmail.com>
2013-01-25 14:36:21 -0800

    Create a security group along with shared mailboxes

--- ExchangeOnlineFunctions.psm1
+++ ExchangeOnlineFunctions.psm1
@@ -27,9 +27,39 @@ function New-SharedMailbox($Alias = (Read-Host -Prompt "Shared Mailbox Alias"),
         return
     }
 
+    # Create the mailbox
+
+    Write-Host "Creating mailbox..."
     New-Mailbox -Name $DisplayName -Alias $Alias -Shared
 
+    Write-Host "Setting quotas on mailbox..."
     Set-Mailbox $Alias -ProhibitSendReceiveQuota 5GB `
                        -ProhibitSendQuota 4.75GB `
-                       -IssueWarningQuota 4.5GB
+                       -IssueWarningQuota 4.5GB `
+                       | Out-Null
+
+    # Set up a security group for granting access to this mailbox
+
+    $GroupDisplayName = "Security Group - $DisplayName"
+    $GroupAlias = "SecurityGroup-$($Alias)"
+
+    Write-Host "Creating security group..."
+    New-DistributionGroup -Name $GroupDisplayName `
+                          -Alias $GroupAlias `
+                          -Type Security `
+                          | Out-Null
+
+    Write-Host "Setting options on the security group..."
+    Set-DistributionGroup $GroupAlias -RequireSenderAuthenticationEnabled $true `
+                                      -AcceptMessagesOnlyFrom $Alias `
+                                      -HiddenFromAddressListsEnabled $true
+
+    Write-Host "Setting permissions on mailbox..."
+    Add-MailboxPermission $DisplayName -User $GroupAlias -AccessRights FullAccess
+    Add-RecipientPermission $DisplayName -Trustee $GroupAlias -AccessRights SendAs
+
+    # Tell the user what we did
+
+    Write-Host "Shared mailbox '$DisplayName' created."
+    Write-Host "Add users as members to the '$GroupDisplayName' distribution group to grant access."
 }
