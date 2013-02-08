33a9450b5e01cbeea51b217922f9061924e53169
Stephen Jennings <Stephen.G.Jennings@gmail.com>
2013-02-08 07:52:59 -0800

    Change Create-SharedMailbox to advanced function
    As a cmdlet it can optionally take pipeline input to process a large
    number of records at once.

--- ExchangeOnlineFunctions.psm1
+++ ExchangeOnlineFunctions.psm1
@@ -8,58 +8,67 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 
-function New-SharedMailbox($Alias = (Read-Host -Prompt "Shared Mailbox Alias"),
-                           $DisplayName = (Read-Host -Prompt "Shared Mailbox Display Name"))
+function New-SharedMailbox
 {
     # .SYNOPSIS
     #
     # Creates a new shared mailbox
 
-    if (!$Alias)
-    {
-        Write-Error "No alias specified."
-        return
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Shared Mailbox Alias")]
+        [Alias("Name")]
+        [string]
+        $Alias,
+
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Shared Mailbox Display Name")]
+        [string]
+        $DisplayName
+    )
+
+    PROCESS {
+
+        if ($pscmdlet.ShouldProcess())
+        {
+            # Create the mailbox
+            Write-Verbose "Creating mailbox..."
+            New-Mailbox -Name $DisplayName -Alias $Alias -Shared
+    
+            Write-Verbose "Setting quotas on mailbox..."
+            Set-Mailbox $Alias -ProhibitSendReceiveQuota 5GB `
+                               -ProhibitSendQuota 4.75GB `
+                               -IssueWarningQuota 4.5GB `
+                               | Out-Null
+    
+            # Set up a security group for granting access to this mailbox
+    
+            $GroupDisplayName = "Security Group - $DisplayName"
+            $GroupAlias = "SecurityGroup-$($Alias)"
+    
+            Write-Verbose "Creating security group..."
+            New-DistributionGroup -Name $GroupDisplayName `
+                                  -Alias $GroupAlias `
+                                  -Type Security `
+                                  | Out-Null
+    
+            Write-Verbose "Setting options on the security group..."
+            Set-DistributionGroup $GroupAlias -RequireSenderAuthenticationEnabled $true `
+                                              -AcceptMessagesOnlyFrom $Alias `
+                                              -HiddenFromAddressListsEnabled $true
+    
+            Write-Verbose "Setting permissions on mailbox..."
+            Add-MailboxPermission $DisplayName -User $GroupAlias -AccessRights FullAccess
+            Add-RecipientPermission $DisplayName -Trustee $GroupAlias -AccessRights SendAs
+    
+            # Tell the user what we did
+    
+            Write-Verbose "Shared mailbox '$DisplayName' created."
+            Write-Verbose "Add users as members to the '$GroupDisplayName' distribution group to grant access."
+        }
+
     }
-
-    if (!$DisplayName)
-    {
-        Write-Error "No display name specified."
-        return
-    }
-
-    # Create the mailbox
-
-    Write-Host "Creating mailbox..."
-    New-Mailbox -Name $DisplayName -Alias $Alias -Shared
-
-    Write-Host "Setting quotas on mailbox..."
-    Set-Mailbox $Alias -ProhibitSendReceiveQuota 5GB `
-                       -ProhibitSendQuota 4.75GB `
-                       -IssueWarningQuota 4.5GB `
-                       | Out-Null
-
-    # Set up a security group for granting access to this mailbox
-
-    $GroupDisplayName = "Security Group - $DisplayName"
-    $GroupAlias = "SecurityGroup-$($Alias)"
-
-    Write-Host "Creating security group..."
-    New-DistributionGroup -Name $GroupDisplayName `
-                          -Alias $GroupAlias `
-                          -Type Security `
-                          | Out-Null
-
-    Write-Host "Setting options on the security group..."
-    Set-DistributionGroup $GroupAlias -RequireSenderAuthenticationEnabled $true `
-                                      -AcceptMessagesOnlyFrom $Alias `
-                                      -HiddenFromAddressListsEnabled $true
-
-    Write-Host "Setting permissions on mailbox..."
-    Add-MailboxPermission $DisplayName -User $GroupAlias -AccessRights FullAccess
-    Add-RecipientPermission $DisplayName -Trustee $GroupAlias -AccessRights SendAs
-
-    # Tell the user what we did
-
-    Write-Host "Shared mailbox '$DisplayName' created."
-    Write-Host "Add users as members to the '$GroupDisplayName' distribution group to grant access."
 }
